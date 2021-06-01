aeaaa16a88c801b2b0f048046a965a72ee2cf1c1
Kind Kidnapper <makogonlyubov@gmail.com>
2021-06-01 23:11:21 +0300

    Home work 12_again

--- HW_12.py
+++ HW_12.py
@@ -2,12 +2,8 @@ import requests
 import csv
 # 1. Написать функцию, которая принимает в виде параметра целое число - количество цитат.
 # и возвращает список не повторяющихся цитат. Если автор не указан, цитату не брать.
-# 2. Написать функцию, которая принимает результат предыдущей функции и сохраняет в csv файл.
-# Имя файла сделать параметром по умолчанию.
-# Заголовки csv файла:
-# Author, Quote, URL.
-# Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
-# Разделитель - запятая.
+
+url = "http://api.forismatic.com/api/1.0/"
 
 def get_data_quotes(number):
     list_quote = []
@@ -18,22 +14,28 @@ def get_data_quotes(number):
                   "lang": "en"}
         r = requests.get(url, params=params)
         data = r.json()
-        if data["quoteAuthor"] == "" or data["quoteText"] in list_quote:
+        if data["quoteAuthor"] == "" or data in list_quote:
             continue
         else:
             list_quote.append([data["quoteAuthor"], data["quoteText"], data["quoteLink"]])
-    def write_quote_csv(filename="quotes.csv"):
-        with open(filename, "w") as csvfile:
-            writer = csv.writer(csvfile)
-            header = ["Author", "Quote", "URL"]
-            sorted_list = sorted(list_quote, key=lambda x: x[0])
-            data_csv = [header] + sorted_list
-            writer.writerows(data_csv)
-    write_quote_csv()
-    return [i[1]+i[0] for i in list_quote]
+    return list_quote
 
 
-url = "http://api.forismatic.com/api/1.0/"
+# 2. Написать функцию, которая принимает результат предыдущей функции и сохраняет в csv файл.
+# Имя файла сделать параметром по умолчанию.
+# Заголовки csv файла:
+# Author, Quote, URL.
+# Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
+# Разделитель - запятая.
 
-list_data = get_data_quotes(2)
-print(list_data)
+def write_quote_csv(number, filename="quotes.csv"):
+    with open(filename, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        header = ["Author", "Quote", "URL"]
+        quote_data = get_data_quotes(number)
+        sorted_list = sorted(quote_data, key=lambda x: x[0])
+        data = [header] + sorted_list
+        writer.writerows(data)
+
+
+write_quote_csv(5)
