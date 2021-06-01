e3a3e0e5819bc1027bd49a3c4b3b0c35d6215690
Kind Kidnapper <makogonlyubov@gmail.com>
2021-06-01 23:27:12 +0300

    Home work 13_v.2

--- HW_13.py
+++ HW_13.py
@@ -2,6 +2,7 @@ class GetDataFromFiles:
     def __init__(self, filename):
         self.filename = filename
         self.data = self.get_data()
+        self.list_date = self.get_list_date()
 
     def get_data(self):
         with open(self.filename, "r") as txt_file:
@@ -18,7 +19,7 @@ class GetDataFromFiles:
         return [line.split(" -")[0] for line in self.data if line[0].isdigit() and line.split(" -")[0][-1].isdigit()]
 
     def get_day(self):
-        list_day = [date.split(" ")[0].rstrip("nsthrd") for date in self.get_list_date()]
+        list_day = [date.split(" ")[0].rstrip("nsthrd") for date in self.list_date]
         day_modified_list = []
         for day in list_day:
             if len(day) == 1:
@@ -40,15 +41,15 @@ class GetDataFromFiles:
                        "October": "10",
                        "November": "11",
                        "December": "12"}
-        list_months = [date.split(" ")[1] for date in self.get_list_date()]
+        list_months = [date.split(" ")[1] for date in self.list_date]
         months_modified_list = [months_dict[months] for months in list_months]
         return months_modified_list
 
     def get_years(self):
-        return [date.split(" ")[2] for date in self.get_list_date()]
+        return [date.split(" ")[2] for date in self.list_date]
 
     def create_list_date_dict(self):
-        date_original = [{"date_original": date} for date in self.get_list_date()]
+        date_original = [{"date_original": date} for date in self.list_date]
         date_modified = ["/".join(data) for data in list(zip(self.get_day(), self.get_months(), self.get_years()))]
         for index, date in enumerate(date_original):
             date["date_modified"] = date_modified[index]
